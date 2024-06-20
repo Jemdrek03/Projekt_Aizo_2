6c1f71a375a392a87921225b3dd082a6f3285e1b
jemdrek:} <j.radlowski03@gmail.com>
2024-06-20 12:09:58 +0200

    Dodanie ostatecznych poprawek i komentarzy

--- MST_Algorythms.h
+++ MST_Algorythms.h
@@ -22,21 +22,21 @@
 struct Edge {
     int u, v, weight;
     bool operator<(const Edge& other) const {
-        return weight < other.weight;
+        return weight < other.weight; // Operator porównania dla sortowania krawędzi według wagi
     }
 };
 
 //Struktura prim
 struct prim {
-    std::vector<int> parent;
-    std::vector<int> key;
-    int vertexes;
+    std::vector<int> parent; // Wektor rodziców w MST
+    std::vector<int> key; // Wektor kluczy (kosztów)
+    int vertexes; // Liczba wierzchołków
 };
 
-//Struktura kruskal
+// Struktura dla wyników algorytmu Kruskala
 struct kruskal{
-    std::vector<Edge> mst;
-    int vertexes;
+    std::vector<Edge> mst; // Wektor krawędzi w MST
+    int vertexes; // Liczba wierzchołków
 };
 
 
@@ -44,112 +44,115 @@ struct kruskal{
 class MST_Algorythms {
 public:
 
-    // Funkcja wykonująca algorytm Prima na liście sąsiedztwa
+    // Algorytm Prima dla listy sąsiedztwa
     static prim primAdjacencyList(const Adjacency_List& graph) {
         prim result;
-        result.vertexes = graph.vertexes;
-        result.parent.resize(graph.vertexes, -1);
-        result.key.resize(graph.vertexes, std::numeric_limits<int>::max());
+        result.vertexes = graph.vertexes; // Ustawienie liczby wierzchołków
+        result.parent.resize(graph.vertexes, -1); // Inicjalizacja wektora rodziców
+        result.key.resize(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora kluczy
 
-        std::vector<bool> inQueue(graph.vertexes, true);
+        std::vector<bool> inQueue(graph.vertexes, true); // Wektor przechowujący informację o wierzchołkach w kolejce
 
-        // Priority queue to store vertices with their key values
+        // Kolejka priorytetowa do przechowywania wierzchołków z ich kluczami
         std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
 
-        // Start from vertex 0
-        result.key[0] = 0;
-        pq.emplace(0, 0); // (key, vertex)
+        result.key[0] = 0; // Klucz startowego wierzchołka ustawiony na 0
+        pq.emplace(0, 0); // Dodanie wierzchołka startowego do kolejki
 
         while (!pq.empty()) {
-            int u = pq.top().second; // Pobieramy wierzchołek z kolejki (drugi element pary)
+            int u = pq.top().second; // Pobranie wierzchołka z kolejki
             pq.pop();
+            inQueue[u] = false; // Ustawienie, że wierzchołek nie jest już w kolejce
 
-            inQueue[u] = false;
-
+            // Przeglądanie sąsiadów wierzchołka
             for (const auto& [v, weight] : graph.list[u]) {
                 if (inQueue[v] && weight < result.key[v]) {
-                    result.parent[v] = u;
-                    result.key[v] = weight;
-                    pq.emplace(weight, v); // Umieszczamy nowy klucz i wierzchołek w kolejce
+                    result.parent[v] = u; // Ustawienie rodzica wierzchołka
+                    result.key[v] = weight; // Aktualizacja klucza
+                    pq.emplace(weight, v); // Dodanie sąsiada do kolejki
                 }
             }
         }
-
         return result;
     }
 
+    // Algorytm Prima dla macierzy incydencji
     static prim primIncidenceMatrix(const Incidence_Matrix& graph) {
         prim result;
-        result.vertexes = graph.vertexes;
-        result.parent.resize(graph.vertexes, -1);
-        result.key.resize(graph.vertexes, std::numeric_limits<int>::max());
+        result.vertexes = graph.vertexes; // Ustawienie liczby wierzchołków
+        result.parent.resize(graph.vertexes, -1); // Inicjalizacja wektora rodziców
+        result.key.resize(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora kluczy
 
-        std::vector<bool> inQueue(graph.vertexes, true);
+        std::vector<bool> inQueue(graph.vertexes, true); // Wektor przechowujący informację o wierzchołkach w kolejce
 
-        // Priority queue to store vertices with their key values
+        // Kolejka priorytetowa do przechowywania wierzchołków z ich kluczami
         std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
 
-        // Start from vertex 0
-        result.key[0] = 0;
-        pq.emplace(0, 0); // (key, vertex)
+        result.key[0] = 0; // Klucz startowego wierzchołka ustawiony na 0
+        pq.emplace(0, 0); // Dodanie wierzchołka startowego do kolejki
 
         while (!pq.empty()) {
-            int u = pq.top().second; // Pobieramy wierzchołek z kolejki (drugi element pary)
+            int u = pq.top().second; // Pobranie wierzchołka z kolejki
             pq.pop();
+            inQueue[u] = false; // Ustawienie, że wierzchołek nie jest już w kolejce
 
-            inQueue[u] = false;
+            // Lista sąsiadów wierzchołka
             std::vector<std::pair<int, int>> Neigh;
 
+            // Przeglądanie wszystkich krawędzi
             for (int j = 0; j < graph.edges; ++j) {
-                if(graph.matrix[u][j] != 0)
-                {
-                    for(int i = 0; i < graph.vertexes; i++)
-                    {
-                        if(i != u && graph.matrix[i][j] != 0)
-                        {
+                if(graph.matrix[u][j] != 0) {
+                    for(int i = 0; i < graph.vertexes; i++) {
+                        if(i != u && graph.matrix[i][j] != 0) {
                             Neigh.push_back({i, abs(graph.matrix[i][j])});
                         }
                     }
                 }
             }
+
+            // Przeglądanie sąsiadów wierzchołka
             for (const auto& [v, weight] : Neigh) {
                 if (inQueue[v] && weight < result.key[v]) {
-                    result.parent[v] = u;
-                    result.key[v] = weight;
-                    pq.emplace(weight, v); // Umieszczamy nowy klucz i wierzchołek w kolejce
+                    result.parent[v] = u; // Ustawienie rodzica wierzchołka
+                    result.key[v] = weight; // Aktualizacja klucza
+                    pq.emplace(weight, v); // Dodanie sąsiada do kolejki
                 }
             }
-
         }
-
         return result;
     }
 
 
 
-    // Kruskal dla listy sąsiedztwa
+    // Algorytm Kruskala dla listy sąsiedztwa
     static kruskal kruskalAdjacencyList(const Adjacency_List& graph) {
         kruskal result;
         std::vector<Edge> edges;
+
+        // Tworzenie wektora krawędzi
         for (int u = 0; u < graph.vertexes; ++u) {
             for (const auto& [v, weight] : graph.list[u]) {
                 edges.push_back({u, v, weight});
             }
         }
 
+        // Sortowanie krawędzi według wagi
         std::sort(edges.begin(), edges.end());
 
+        // Inicjalizacja wektorów rodziców i rang
         std::vector<int> parent(graph.vertexes);
         std::iota(parent.begin(), parent.end(), 0);
-
         std::vector<int> rank(graph.vertexes, 0);
 
+        // Wektor krawędzi w MST
         std::vector<Edge> mst;
 
+        // Przeglądanie krawędzi
         for (const auto& edge : edges) {
             int u = find(parent, edge.u);
             int v = find(parent, edge.v);
 
+            // Dodanie krawędzi do MST, jeśli nie tworzy cyklu
             if (u != v) {
                 mst.push_back(edge);
                 unionSets(parent, rank, u, v);
@@ -162,10 +165,12 @@ public:
         return result;
     }
 
-    // Kruskal dla macierzy incydencji
+    // Algorytm Kruskala dla macierzy incydencji
     static kruskal kruskalIncidenceMatrix(const Incidence_Matrix& graph) {
         kruskal result;
         std::vector<Edge> edges;
+
+        // Tworzenie wektora krawędzi
         for (int j = 0; j < graph.edges; ++j) {
             int u = -1, v = -1, weight = -1;
             for (int i = 0; i < graph.vertexes; ++i) {
@@ -181,19 +186,23 @@ public:
             }
         }
 
+        // Sortowanie krawędzi według wagi
         std::sort(edges.begin(), edges.end());
 
+        // Inicjalizacja wektorów rodziców i rang
         std::vector<int> parent(graph.vertexes);
         std::iota(parent.begin(), parent.end(), 0);
-
         std::vector<int> rank(graph.vertexes, 0);
 
+        // Wektor krawędzi w MST
         std::vector<Edge> mst;
 
+        // Przeglądanie krawędzi
         for (const auto& edge : edges) {
             int u = find(parent, edge.u);
             int v = find(parent, edge.v);
 
+            // Dodanie krawędzi do MST, jeśli nie tworzy cyklu
             if (u != v) {
                 mst.push_back(edge);
                 unionSets(parent, rank, u, v);
@@ -208,6 +217,7 @@ public:
 
 
 private:
+    // Znajdowanie korzenia drzewa dla danego wierzchołka
     static int find(std::vector<int>& parent, int i) {
         if (parent[i] != i) {
             parent[i] = find(parent, parent[i]);
@@ -215,14 +225,29 @@ private:
         return parent[i];
     }
 
+    // Łączenie dwóch zbiorów w jeden
+// parent - wektor przechowujący rodziców wierzchołków
+// rank - wektor przechowujący rangi wierzchołków
+// u - pierwszy wierzchołek
+// v - drugi wierzchołek
     static void unionSets(std::vector<int>& parent, std::vector<int>& rank, int u, int v) {
+        // Znajdź korzenie zbiorów, do których należą wierzchołki u i v
         int rootU = find(parent, u);
         int rootV = find(parent, v);
+
+        // Jeśli ranga korzenia u jest mniejsza niż ranga korzenia v
         if (rank[rootU] < rank[rootV]) {
+            // Ustaw korzeń u jako potomka korzenia v
             parent[rootU] = rootV;
-        } else if (rank[rootU] > rank[rootV]) {
+        }
+            // Jeśli ranga korzenia u jest większa niż ranga korzenia v
+        else if (rank[rootU] > rank[rootV]) {
+            // Ustaw korzeń v jako potomka korzenia u
             parent[rootV] = rootU;
-        } else {
+        }
+            // Jeśli rangi korzeni u i v są równe
+        else {
+            // Ustaw korzeń v jako potomka korzenia u i zwiększ rangę korzenia u
             parent[rootV] = rootU;
             rank[rootU]++;
         }
--- Shorthest_Path_Algorythms.h
+++ Shorthest_Path_Algorythms.h
@@ -19,60 +19,62 @@ class Shortest_Path_Algorythms {
 public:
     // Dijkstra dla listy sąsiedztwa
     static std::pair<std::vector<int>, int> dijkstraAdjacencyList(const Adjacency_List& graph, int start, int end) {
-        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max());
-        std::vector<int> previous(graph.vertexes, -1);
-        distances[start] = 0;
+        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora odległości maksymalnymi wartościami
+        std::vector<int> previous(graph.vertexes, -1); // Wektor przechowujący poprzednie wierzchołki na ścieżce
+        distances[start] = 0; // Odległość do wierzchołka startowego wynosi 0
 
-        using pii = std::pair<int, int>;
-        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
-        pq.emplace(0, start);
+        using pii = std::pair<int, int>; // Skrócenie zapisu pary int, int
+        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq; // Kolejka priorytetowa do przechowywania wierzchołków do odwiedzenia
+        pq.emplace(0, start); // Dodanie wierzchołka startowego do kolejki
 
         while (!pq.empty()) {
-            int dist = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
+            int dist = pq.top().first; // Pobranie odległości do wierzchołka
+            int u = pq.top().second; // Pobranie wierzchołka
+            pq.pop(); // Usunięcie wierzchołka z kolejki
 
-            if (dist > distances[u]) continue;
+            if (dist > distances[u]) continue; // Jeżeli odległość jest większa niż zarejestrowana, przejdź dalej
 
             for (const auto& [v, weight] : graph.list[u]) {
+                // Jeśli znaleziono krótszą ścieżkę do sąsiada
                 if (distances[u] + weight < distances[v]) {
-                    distances[v] = distances[u] + weight;
-                    previous[v] = u;
-                    pq.emplace(distances[v], v);
+                    distances[v] = distances[u] + weight; // Aktualizacja odległości do sąsiada
+                    previous[v] = u; // Ustawienie poprzedniego wierzchołka
+                    pq.emplace(distances[v], v); // Dodanie sąsiada do kolejki
                 }
             }
         }
 
+        // Zwrócenie znalezionej ścieżki oraz jej łącznej wagi
         return reconstructPath(previous, distances[end], start, end);
     }
 
     // Dijkstra dla macierzy incydencji
     static std::pair<std::vector<int>, int> dijkstraIncidenceMatrix(const Incidence_Matrix& graph, int start, int end) {
-        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max());
-        std::vector<int> previous(graph.vertexes, -1);
-        distances[start] = 0;
+        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora odległości maksymalnymi wartościami
+        std::vector<int> previous(graph.vertexes, -1); // Wektor przechowujący poprzednie wierzchołki na ścieżce
+        distances[start] = 0; // Odległość do wierzchołka startowego wynosi 0
 
-        using pii = std::pair<int, int>;
-        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
-        pq.emplace(0, start);
+        using pii = std::pair<int, int>; // Skrócenie zapisu pary int, int
+        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq; // Kolejka priorytetowa do przechowywania wierzchołków do odwiedzenia
+        pq.emplace(0, start); // Dodanie wierzchołka startowego do kolejki
 
         while (!pq.empty()) {
-            int dist = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
+            int dist = pq.top().first; // Pobranie odległości do wierzchołka
+            int u = pq.top().second; // Pobranie wierzchołka
+            pq.pop(); // Usunięcie wierzchołka z kolejki
 
-            if (dist > distances[u]) continue;
+            if (dist > distances[u]) continue; // Jeżeli odległość jest większa niż zarejestrowana, przejdź dalej
 
             for (int j = 0; j < graph.edges; ++j) {
-                if (graph.matrix[u][j] > 0) {
-                    int weight = graph.matrix[u][j];
-                    for (int i = 0; i < graph.vertexes; ++i) {
-                        if (i != u && graph.matrix[i][j] != 0) {
-                            int v = i;
+                if (graph.matrix[u][j] > 0) { // Sprawdzenie, czy wierzchołek jest połączony z krawędzią
+                    int weight = graph.matrix[u][j]; // Pobranie wagi krawędzi
+                    for (int i = 0; i < graph.vertexes; ++i) { // Przejście przez wszystkie wierzchołki
+                        if (i != u && graph.matrix[i][j] != 0) { // Sprawdzenie, czy wierzchołek i jest połączony z krawędzią
+                            int v = i; // Pobranie wierzchołka sąsiedniego
                             if (distances[u] + weight < distances[v]) {
-                                distances[v] = distances[u] + weight;
-                                previous[v] = u;
-                                pq.emplace(distances[v], v);
+                                distances[v] = distances[u] + weight; // Aktualizacja odległości do sąsiada
+                                previous[v] = u; // Ustawienie poprzedniego wierzchołka
+                                pq.emplace(distances[v], v); // Dodanie sąsiada do kolejki
                             }
                         }
                     }
@@ -83,41 +85,48 @@ public:
         return reconstructPath(previous, distances[end], start, end);
     }
 
-    // Ford-Bellman dla listy sąsiedztwa
+    // Algorytm Forda-Bellmana dla listy sąsiedztwa
     static std::pair<std::vector<int>, int> bellmanFordAdjacencyList(const Adjacency_List& graph, int start, int end) {
-        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max());
-        std::vector<int> previous(graph.vertexes, -1);
-        distances[start] = 0;
+        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora odległości maksymalnymi wartościami
+        std::vector<int> previous(graph.vertexes, -1); // Wektor przechowujący poprzednie wierzchołki na ścieżce
+        distances[start] = 0; // Odległość do wierzchołka startowego wynosi 0
 
+        // Główna pętla algorytmu Forda-Bellmana
         for (int i = 0; i < graph.vertexes - 1; ++i) {
             for (int u = 0; u < graph.vertexes; ++u) {
+                // Przejście przez sąsiadów wierzchołka
                 for (const auto& [v, weight] : graph.list[u]) {
+                    // Jeśli znaleziono krótszą ścieżkę do sąsiada
                     if (distances[u] != std::numeric_limits<int>::max() && distances[u] + weight < distances[v]) {
-                        distances[v] = distances[u] + weight;
-                        previous[v] = u;
+                        distances[v] = distances[u] + weight; // Aktualizacja odległości do sąsiada
+                        previous[v] = u; // Ustawienie poprzedniego wierzchołka
                     }
                 }
             }
         }
 
+        // Zwrócenie znalezionej ścieżki oraz jej łącznej wagi
         return reconstructPath(previous, distances[end], start, end);
     }
 
-    // Ford-Bellman dla macierzy incydencji
-    static std::pair<std::vector<int>, int> bellmanFordIncidenceMatrix(const Incidence_Matrix& graph, int start, int end) {
-        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max());
-        std::vector<int> previous(graph.vertexes, -1);
-        distances[start] = 0;
 
+    // Algorytm Forda-Bellmana dla macierzy incydencji
+    static std::pair<std::vector<int>, int> bellmanFordIncidenceMatrix(const Incidence_Matrix& graph, int start, int end) {
+        std::vector<int> distances(graph.vertexes, std::numeric_limits<int>::max()); // Inicjalizacja wektora odległości maksymalnymi wartościami
+        std::vector<int> previous(graph.vertexes, -1); // Wektor przechowujący poprzednie wierzchołki na ścieżce
+        distances[start] = 0; // Odległość do wierzchołka startowego wynosi 0
+
+        // Główna pętla algorytmu Forda-Bellmana
         for (int i = 0; i < graph.vertexes - 1; ++i) {
             for (int j = 0; j < graph.edges; ++j) {
                 for (int u = 0; u < graph.vertexes; ++u) {
-                    if (graph.matrix[u][j] > 0) {
-                        int weight = graph.matrix[u][j];
-                        for (int v = 0; v < graph.vertexes; ++v) {
+                    if (graph.matrix[u][j] > 0) { // Sprawdzenie, czy wierzchołek jest połączony z krawędzią
+                        int weight = graph.matrix[u][j]; // Pobranie wagi krawędzi
+                        for (int v = 0; v < graph.vertexes; ++v) { // Przejście przez wszystkie wierzchołki
+                            // Jeśli znaleziono krótszą ścieżkę do sąsiada
                             if (graph.matrix[v][j] != 0 && distances[u] != std::numeric_limits<int>::max() && distances[u] + weight < distances[v]) {
-                                distances[v] = distances[u] + weight;
-                                previous[v] = u;
+                                distances[v] = distances[u] + weight; // Aktualizacja odległości do sąsiada
+                                previous[v] = u; // Ustawienie poprzedniego wierzchołka
                             }
                         }
                     }
@@ -125,6 +134,7 @@ public:
             }
         }
 
+        // Zwrócenie znalezionej ścieżki oraz jej łącznej wagi
         return reconstructPath(previous, distances[end], start, end);
     }
 
